0d2c55428771e41b912d7ce52286adcc2c6f5579
qiang4.wang <qiang4.wang@changhong.com>
2019-05-20 10:49:06 +0800

    info

---
+++ src/ajax/AxiosConfig.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import 'nprogress/nprogress.css'
+import "./progress/Progress.css"
+import NProgress from 'nprogress'
+
+axios.interceptors.request.use(config => {
+    NProgress.start();
+    return config;
+}, error => {
+    NProgress.done();
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+    NProgress.done();
+    if (200 === response.status) {
+        if ("-0401" === response.data.code) {
+            window.top.location = "/login";
+            return;
+        }
+    } else {
+        console.error("http status is not 200. ", response);
+    }
+    return response;
+}, error => {
+    NProgress.done();
+    return Promise.reject(error);
+});
+
+export default {
+    baseURl: "/server"
+}
